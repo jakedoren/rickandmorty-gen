1fea560660e346b781ec632480388c6efe7a3703
jakedoren <74628634+jakedoren@users.noreply.github.com>
2021-01-29 19:04:45 -0500

    housekeeping

--- src/App.js
+++ src/App.js
@@ -2,39 +2,39 @@ import React, { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  const [quotes, setQuotes] = useState('')
+  const [character, setCharacter] = useState('')
 
-  let getQuotes = () => {
+  let getCharacter = () => {
     let randomNumb = Math.floor(Math.random() * 20)
     fetch("https://rickandmortyapi.com/api/character")
       .then((res) => res.json())
-      .then((data) => setQuotes(data.results[randomNumb]))
+      .then((data) => setCharacter(data.results[randomNumb]))
   }
 
   const statusStyle = {
     color: "white"
   }
 
-  if(quotes.status === "Dead") {
+  if(character.status === "Dead") {
     statusStyle.color = "red"
-  } else if(quotes.status === "Alive") {
+  } else if(character.status === "Alive") {
     statusStyle.color = "#a4ca51"
   }
 
 
   return (
     <div className="App">
-      <div className="quotes-container">
-          <h1 className="quotes">{quotes.name}</h1>
-          {quotes.status ? 
+      <div className="character-container">
+          <h1 className="characters">{character.name}</h1>
+          {character.status ? 
           <div className="character-info">
-            <p>status: <span style={statusStyle}>{quotes.status}</span></p>
-            <p>species: {quotes.species}</p>
-            <p>gender: {quotes.gender}</p>
-          </div> : <h1 className="quotes">Click the button below to generate a Rick and Morty character!</h1>}
+            <p>status: <span style={statusStyle}>{character.status}</span></p>
+            <p>species: {character.species}</p>
+            <p>gender: {character.gender}</p>
+          </div> : <h1 className="character">Click the button below to generate a Rick and Morty character!</h1>}
       </div>
-      <div className="get-quote">
-        <button className="btn" onClick={getQuotes}>Get Character</button>
+      <div className="get-character">
+        <button className="btn" onClick={getCharacter}>Get Character</button>
       </div>
     </div>
   )
